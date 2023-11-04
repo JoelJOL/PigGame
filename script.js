67923716b527312466f22ebbbcb424b2a55f0a21
Abraham Jacob Kurialanickal <abraham.k@experionglobal.local>
2023-11-04 16:19:25 +0530

    feat:- accepted roll dice function from Gopika

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 function rollButton() {
     var rollValue = Math.floor((Math.random() * 6) + 1);
 	var diceLocation = "/PigGame/images/" + "dice-" + rollValue + ".png";
@@ -9,3 +10,14 @@ function rollButton() {
 'use strict';
 //TEST
 >>>>>>> b4452e1 (Testing first commit)
+=======
+//'use strict';
+
+function ChangePlayer() {
+  let sectionClass = document.getElementsByTagName('section');
+  console.log(sectionClass);
+  for (let i = 0; i < 2; i++) {
+    sectionClass[i].classList.toggle('player--active');
+  }
+}
+>>>>>>> dfb5f4d (feat: Change active player background on clicking HOLD)
