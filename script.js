dfb5f4daad987d7702f31a748ea7bfaaa70f9148
Abraham Jacob Kurialanickal <abraham.k@experionglobal.local>
2023-11-04 12:46:56 +0530

    feat: Change active player background on clicking HOLD

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-'use strict';
-//TEST
+//'use strict';
+
+function ChangePlayer() {
+  let sectionClass = document.getElementsByTagName('section');
+  console.log(sectionClass);
+  for (let i = 0; i < 2; i++) {
+    sectionClass[i].classList.toggle('player--active');
+  }
+}
