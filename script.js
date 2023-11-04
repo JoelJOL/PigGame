353c0c86dc6c0d1b9d9d4dbbb35dc64da4c43fbf
Abraham Jacob Kurialanickal <abraham.k@experionglobal.local>
2023-11-04 11:37:21 +0530

    Testing first commit

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 function rollButton() {
     var rollValue = Math.floor((Math.random() * 6) + 1);
 	var diceLocation = "/PigGame/images/" + "dice-" + rollValue + ".png";
 	var img = document.querySelectorAll("img")[0];
 	img.setAttribute("src", diceLocation);
   }
+=======
+'use strict';
+//TEST
+>>>>>>> b4452e1 (Testing first commit)
