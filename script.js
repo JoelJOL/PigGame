c6020558bc741468343f56a07cee5ab2715641a2
Joel Jose <joel.jose@experionglobal.local>
2023-11-04 15:27:32 +0530

    feat:- Added function to calculate score

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+'use strict';
 function rollButton() {
     var rollValue = Math.floor((Math.random() * 6) + 1);
 	var diceLocation = "/PigGame/images/" + "dice-" + rollValue + ".png";
 	var img = document.querySelectorAll("img")[0];
 	img.setAttribute("src", diceLocation);
   }
+
+  'use strict';
+  let score=10;
+  let currentScore=0;
+  const getScorePlayer1=(score)=>{
+      currentScore+=score;
+      let scoreTag=document.getElementById("current--0");
+      console.log(scoreTag);
+      scoreTag.textContent=`${currentScore}`;
+  }
+  getScorePlayer1(score);
+  
+  
+  const getScorePlayer2=(score)=>{
+      currentScore+=score;
+      let scoreTag=document.getElementById("current--1");
+      console.log(scoreTag);
+      scoreTag.textContent=`${currentScore}`;
+  }
+  getScorePlayer2(score);
