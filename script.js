3430b75fb014ad5aab460cb3d9026071d899289e
Joel Jose <joel.jose@experionglobal.local>
2023-11-04 15:27:32 +0530

    feat:- Added function to calculate score

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 'use strict';
+let score=10;
+let currentScore=0;
+const getScorePlayer1=(score)=>{
+    currentScore+=score;
+    let scoreTag=document.getElementById("current--0");
+    console.log(scoreTag);
+    scoreTag.textContent=`${currentScore}`;
+}
+getScorePlayer1(score);
 
 
-
+const getScorePlayer2=(score)=>{
+    currentScore+=score;
+    let scoreTag=document.getElementById("current--1");
+    console.log(scoreTag);
+    scoreTag.textContent=`${currentScore}`;
+}
+getScorePlayer2(score);
