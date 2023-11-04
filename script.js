f045671ef02808ca625a958156b9744ed88bb59b
Joel Jose <joel.jose@experionglobal.local>
2023-11-04 23:10:54 +0530

    feat:- dice roll and score

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 'use strict';
-<<<<<<< HEAD
 function rollButton() {
     var rollValue = Math.floor((Math.random() * 6) + 1);
 	var diceLocation = "/PigGame/images/" + "dice-" + rollValue + ".png";
 	var img = document.querySelectorAll("img")[0];
 	img.setAttribute("src", diceLocation);
+    getScorePlayer1(rollValue);
   }
-
-  'use strict';
-  let score=10;
+  
+  let score=0;
   let currentScore=0;
   const getScorePlayer1=(score)=>{
       currentScore+=score;
@@ -26,23 +25,3 @@ function rollButton() {
       scoreTag.textContent=`${currentScore}`;
   }
   getScorePlayer2(score);
-=======
-let score=10;
-let currentScore=0;
-const getScorePlayer1=(score)=>{
-    currentScore+=score;
-    let scoreTag=document.getElementById("current--0");
-    console.log(scoreTag);
-    scoreTag.textContent=`${currentScore}`;
-}
-getScorePlayer1(score);
-
-
-const getScorePlayer2=(score)=>{
-    currentScore+=score;
-    let scoreTag=document.getElementById("current--1");
-    console.log(scoreTag);
-    scoreTag.textContent=`${currentScore}`;
-}
-getScorePlayer2(score);
->>>>>>> 3430b75fb014ad5aab460cb3d9026071d899289e
