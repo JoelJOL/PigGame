6f220d7169117e23e4b9b6bed18601d967df54b2
Abraham Jacob Kurialanickal <abraham.k@experionglobal.local>
2023-11-06 18:17:45 +0530

    fix:- changed the function definition

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ function rollButton() {
   img.setAttribute('src', diceLocation);
 }
 
-let playerId;
+let playerId = 0;
 function ChangePlayer() {
   let sectionClass = document.getElementsByTagName('section');
-  for (playerId = 0; playerId < 2; playerId++) {
-    if (sectionClass[playerId].classList.contains('player--active')) {
-      let activeScore = document.getElementById(
-        `current--${playerId}`
-      ).innerHTML;
-      console.log(activeScore);
-      document.getElementById(`score--${playerId}`).innerHTML = activeScore;
+  for (i = 0; i < 2; i++) {
+    if (sectionClass[i].classList.contains('player--active')) {
+      let activeScore = document.getElementById(`current--${i}`).innerHTML;
+      document.getElementById(`score--${i}`).innerHTML = activeScore;
+      sectionClass[i].classList.remove('player--active');
+    } else {
+      sectionClass[i].classList.add('player--active');
+      playerId = i;
     }
-    sectionClass[playerId].classList.toggle('player--active');
   }
 }
