57b4b3291a6e332f9b84c7d5ce796a4153e3d947
Joel Jose <63315432+JoelJOL@users.noreply.github.com>
2023-11-06 22:39:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -57,26 +57,3 @@ const getScorePlayer=(score)=>{
     }
 
   }
-   
-  
- 
-
-  
-  
-   function checkwinner()
-   {
-    let playerId=0;
-    let winnerscore=document.getElementsByClassName('player--active');
-    console.log(winnerscore)
-    let score=document.getElementById(`score--${playerId}`);
-    winScore=score.textContent;
-    console.log(winScore)
-    if(winScore>=100)
-    {
-     winnerscore[0].classList.add('player--winner');
-    }
-
-  }
-   
-  
- 
