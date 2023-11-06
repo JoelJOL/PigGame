35c6e42576342a61002a91d05808f427b73d4c05
Varkey E Njaliath <varkey.njaliath@experionglobal.local>
2023-11-06 17:03:32 +0530

    feat:- check winner function added

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-'use strict';
+
+  
+  
+   function checkwinner()
+   {
+    let playerId=0;
+    let winnerscore=document.getElementsByClassName('player--active');
+    console.log(winnerscore)
+    let score=document.getElementById(`score--${playerId}`);
+    winScore=score.textContent;
+    console.log(winScore)
+    if(winScore>=100)
+    {
+     winnerscore[0].classList.add('player--winner');
+    }
+
+  }
+   
+  
+ 
